5753a09e986b9ecbbb370a3c2a27b92910ff8442
jaden698 <mahfoud.jed@gmail.com>
2021-12-05 19:20:45 +0100

    cm

--- src/main/java/com/example/crudsp/Controller/TutorialController.java
+++ src/main/java/com/example/crudsp/Controller/TutorialController.java
@@ -38,16 +38,7 @@ public class TutorialController {
         }
     }
 
-    @GetMapping("/tutorials/{id}")
-    public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") String id) {
-        Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
 
-        if (tutorialData.isPresent()) {
-            return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
     @PostMapping("/tutorials")
     public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
         try {
